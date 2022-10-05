beca3ecb56cd8324c00a6b80d1a29ae4cdc14da0
FABRICE SERAPION <serapion.fabrice@hotmail.fr>
2022-10-05 16:29:06 +0200

    sécurisation

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start() ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,33 +14,51 @@
   <form action='thanks.php' method='post'>
     <div>
       <label for='firstname'>Prénom :</label>
-      <input type='text' id='firstname' name='firstname'>
+      <?php if (isset($_GET["firstname"])) : ?>
+        <p>Veuillez fournir votre prénom</p>
+      <?php endif; ?>
+      <input type='text' id='firstname' name='firstname' value='<?= $_SESSION["firstname"] ?? '' ?>'>
     </div>
     <div>
       <label for='lastname'>Nom :</label>
-      <input type='text' id='lastname' name='lastname'>
+      <?php if (isset($_GET["lastname"])) : ?>
+        <p>Veuillez fournir votre nom</p>
+      <?php endif; ?>
+      <input type='text' id='lastname' name='lastname' value='<?= $_SESSION["lastname"] ?? '' ?>'>
     </div>
     <div>
       <label for='courriel'>Courriel :</label>
-      <input type='email' id='courriel' name='user_email'>
+      <?php if (isset($_GET["email"])) : ?>
+        <p>Veuillez fournir votre email</p>
+      <?php endif; ?>
+      <input type='email' id='email' name='email' value='<?= $_SESSION["email"] ?? '' ?>'>
     </div>
     <div>
       <label for='phone'>N° de téléphone</label>
-      <input type='tel' id='phone' name='phone' pattern='[0-9]{10}'>
+      <?php if (isset($_GET["phone"])) : ?>
+        <p>Veuillez fournir votre N° de téléphone</p>
+      <?php endif; ?>
+      <input type='tel' id='phone' name='phone' pattern='[0-9]{10}' value='<?= $_SESSION["phone"] ?? '' ?>'>
     </div>
     <div>
       <label for='subject'>Sujet du message : </label>
-      <select id='subject' name='subject_message'>
-        <option value=''>...</option>
-        <option value='wild code school'>Wild code school</option>
-        <option value='le sens de la vie'>Retards/Absences</option>
-        <option value='indiana jones'>Checkpoints</option>
-        <option value='php'>Quêtes</option>
+      <?php if (isset($_GET["subject"])) : ?>
+        <p>Veuillez fournir un sujet à votre message</p>
+      <?php endif; ?>
+      <select id='subject' name='subject'>
+        <option>...</option>
+        <option value='wild code school' <?php if (isset($_SESSION["subject"]) && $_SESSION["subject"] == "wild code school") : ?> selected <?php endif; ?>>Wild code school</option>
+        <option value='Retards/Absences' <?php if (isset($_SESSION["subject"]) && $_SESSION["subject"] == "Retards/Absences") : ?> selected <?php endif; ?>>Retards/Absences</option>
+        <option value='Checkpoints' <?php if (isset($_SESSION["subject"]) && $_SESSION["subject"] == "Checkpoints") : ?> selected <?php endif; ?>>Checkpoints</option>
+        <option value='Quêtes' <?php if (isset($_SESSION["subject"]) && $_SESSION["subject"] == "Quêtes") : ?> selected <?php endif; ?>>Quêtes</option>
       </select>
     </div>
     <div>
       <label for='message'>Message :</label>
-      <textarea id='message' name='user_message'></textarea>
+      <?php if (isset($_GET["message"])) : ?>
+        <p>Veuillez expliciter votre demande</p>
+      <?php endif; ?>
+      <textarea id='message' name='message'></textarea>
     </div>
     <div class='button'>
       <button type='submit'>Envoyer votre message</button>
--- thanks.php
+++ thanks.php
@@ -1,4 +1,66 @@
 <?php
-echo "Merci $_POST[firstname] $_POST[lastname] de nous avoir contacté à propos de “$_POST[subject_message]”.<br><br>
-Un de nos conseiller vous contactera soit à l’adresse $_POST[user_email] ou par téléphone au $_POST[phone] dans les plus brefs délais pour traiter votre demande :<br><br>
-$_POST[user_message]" . PHP_EOL;
+
+session_start();
+
+
+function sanitizeInput($inserted_input)
+{
+    return htmlspecialchars(trim(stripslashes($inserted_input)));
+}
+
+$firstname = $lastname = $email = $subject = $phone = $message = "";
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_SESSION = $_POST;
+    if (!isset($_POST['firstname']) || trim($_POST['firstname']) === '') {
+        $errors[] = "firstname=1";
+    } else {
+        $firstname = sanitizeInput($_POST['firstname']);
+    }
+
+    if (!isset($_POST['lastname']) || trim($_POST['lastname']) === '') {
+        $errors[] = "lastname=1";
+    } else {
+        $lastname = sanitizeInput($_POST['lastname']);
+    }
+
+    if (!isset($_POST['email']) || trim($_POST['email']) === '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "email=1";
+    } else {
+        $email = sanitizeInput($_POST['email']);
+    }
+
+    if (!isset($_POST['phone']) || trim($_POST['phone']) === '') {
+        $errors[] = "phone=1";
+    } else {
+        $phone = sanitizeInput($_POST['phone']);
+    }
+
+    if (!isset($_POST['subject']) || trim($_POST['subject']) === '' || $_POST['subject'] === '...') {
+        $errors[] = "subject=1";
+    } else {
+        $subject = sanitizeInput($_POST['phone']);
+    }
+
+    if (!isset($_POST['message']) || trim($_POST['message']) === '') {
+        $errors[] = "message=1";
+    } else {
+        $message = sanitizeInput($_POST['phone']);
+    }
+}
+
+
+if (!empty($errors)) {
+    // traitement du formulaire
+    // puis redirection
+    $errorsJoined = join('&', $errors);
+    header("Location: index.php?" . $errorsJoined);
+} else {
+    echo "Merci $_POST[firstname] $_POST[lastname] de nous avoir contacté à propos de “$_POST[subject]”.<br><br>
+    Un de nos conseiller vous contactera soit à l’adresse $_POST[email] ou par téléphone au $_POST[phone] dans les plus brefs délais pour traiter votre demande :<br><br>
+    $_POST[message]" . PHP_EOL;
+
+    echo "<br><a href=index.php>Envoyer un autre message</a>";
+}
